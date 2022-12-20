14f336083401c029c1a5c5fac36cbf3ef81a5011
Foxlove <116038981+jadegfox@users.noreply.github.com>
2022-12-20 00:25:14 +0000

    Fishing Yay

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', buzzfeedQuiz, false);
 
-function buzzfeedQuiz() { 
-}
+function buzzfeedQuiz(){ 
+
   
   // task 4: declare variables for the html elements you'll use as selectors later. You need the submit button, display div, and all three inputs for your questions.
 
@@ -24,9 +24,9 @@ function buzzfeedQuiz() {
   button.onclick = function(){
 
     // task 6: Declare user input variables for each of your questions. If you have three questions, you should have three variables. These variables should equal the value of the user input for each question. 
-let options1 = q1.val();  
-let options2 = q2.val();  
-let options3 = q3.val(); 
+let options1 = q1.value;  
+let options2 = q2.value;  
+let options3 = q3.value; 
 
 
 
@@ -70,16 +70,31 @@ if (options3 === "I don’t play the game"){
 
     // task 10: declare a variable for the total score of the quiz and set it equal to the sum of each question score.
 let totalQuiz = q3Score + q2Score + q1Score;
-
+console.log (totalQuiz);
 
 
     // task 11: create a conditional statement for the result to the quiz based on the total score. Using string interpolation, display these to the page: the result, an image that corresponds and a message to go with it.
- if(totalQuiz > 1 &&totalQuiz < 5){
-   displayResult.innerHTML = "your " ; 
+ if(totalQuiz > 1 && totalQuiz < 5){
+   displayResult.innerHTML = "your a water vision."; 
  let waterImg = document.createElement("img"); 
-   waterImg.src= "https://ih1.redbubble.net/image.1937324613.2520/st,small,507x507-pad,600x600,f8f8f8.jpg"
- }
-
-
+   waterImg.src= "https://ih1.redbubble.net/image.1937324613.2520/st,small,507x507-pad,600x600,f8f8f8.jpg";  
+ displayResult.insertAdjacentElement('afterend',waterImg);
+ }else if(totalQuiz > 5 && totalQuiz < 7){
+   displayResult.innerHTML = "your a Fire vision";  
+   let fireImg = document.createElement("img"); 
+   fireImg.src= "https://ih1.redbubble.net/image.2361714614.7191/st,small,507x507-pad,600x600,f8f8f8.jpg"; 
+displayResult.insertAdjacentElement('afterend',fireImg);
+ }else if(totalQuiz > 7 && totalQuiz < 10){
+   displayResult.innerHTML = "your a Earth vision. ";  
+   let earthImg = document.createElement("img") 
+   earthImg.src= "https://static.wikia.nocookie.net/gensin-impact/images/9/94/Item_Liyue_Vision.png/revision/latest?cb=20210703160038";
+   displayResult.insertAdjacentElement('afterend',earthImg); 
+   }else if(totalQuiz > 10 && totalQuiz < 13){
+   displayResult.innerHTML = "your a Ice vision. ";  
+   let iceImg = document.createElement("img") 
+   iceImg.src= "https://ih1.redbubble.net/image.1937330499.2698/st,small,507x507-pad,600x600,f8f8f8.jpg";
+   displayResult.insertAdjacentElement('afterend',iceImg);
   };
-}
+}  
+} 
+//monospace 
